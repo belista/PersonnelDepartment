d08b6bf6e16fc88f3efd8a1aac65f0d08bf27943
belista <shestakov9999@mail.ru>
2017-11-30 16:45:03 +0300

    Добавлен DelegateCommand.

---
+++ PersonnelDepartment/Mvvm/DelegateCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PersonnelDepartment.Mvvm
+{
+    class DelegateCommand : ICommand
+    {
+        #region Fields
+        private Action _action;
+        #endregion
+
+
+        public DelegateCommand(Action action) =>
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+
+
+        #region ICommand implementation
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            try
+            {
+                _action();
+            }
+            catch (Exception)
+            {
+                Debugger.Break();
+            }
+        }
+        #endregion
+    }
+}
