b65b29ac66467e679f54bcdf1f4dd473c91343d9
belista <shestakov9999@mail.ru>
2017-11-30 16:31:14 +0300

    Добавлен EmployeeService.

---
+++ PersonnelDepartment/Core/Services/DataProvider/EmployeeService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonnelDepartment.Models;
+using System.Data.Entity;
+using System.Diagnostics;
+
+namespace PersonnelDepartment.Core.Services.DataProvider
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private EmployeeContext _db;
+        public EmployeeService(EmployeeContext db)
+        {
+            _db = db;
+            db.Employees.Load();
+        }
+
+        public Task<bool> AddAsync(Employee employee)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    if (employee != null)
+                    {
+                        _db.Employees.Add(employee);
+                        _db.SaveChanges();
+                        return true;
+                    }
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    Debug.Fail(ex.Message);
+                    return false;
+                }
+            });
+        }
+
+        public Task<List<Employee>> GetAsync()
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    var employees = _db.Employees.ToList();
+                    if (employees == null)
+                    {
+                        return null;
+                    }
+
+                    return employees;
+                }
+                catch (Exception ex)
+                {
+                    Debug.Fail(ex.Message);
+                    return null;
+                }
+            });
+        }
+
+        public Task<Employee> GetById(int id)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    var employee = _db.Employees.SingleOrDefault(u => u.Id == id);
+
+                    if (employee == null)
+                    {
+                        return null;
+                    }
+
+                    return employee;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            });
+        }
+
+        public Task<bool> RemoveAsync(Employee employee)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    if (employee != null)
+                    {
+                        _db.Employees.Remove(employee);
+                        _db.SaveChanges();
+                        return true;
+                    }
+
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    Debug.Fail(ex.Message);
+                    return false;
+                }
+            });
+        }
+    }
+}
---
+++ PersonnelDepartment/Core/Services/DataProvider/IEmployeeService.cs
@@ -0,0 +1,17 @@
+﻿using PersonnelDepartment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.Core.Services.DataProvider
+{
+    public interface IEmployeeService
+    {
+        Task<List<Employee>> GetAsync();
+        Task<bool> RemoveAsync(Employee user);
+        Task<bool> AddAsync(Employee user);
+        Task<Employee> GetById(int id);
+    }
+}
