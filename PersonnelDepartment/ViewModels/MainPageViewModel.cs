5d0ec6e2de238155bfa3f6e89f30b5d2eeab260b
belista <shestakov9999@mail.ru>
2017-11-30 16:49:26 +0300

    Добавлены View и ViewModel.

---
+++ PersonnelDepartment/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PersonnelDepartment.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonnelDepartment.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+
+    }
+}
---
+++ PersonnelDepartment/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PersonnelDepartment.ViewModels;
+using System.Windows.Controls;
+
+namespace PersonnelDepartment.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DataContext = new MainPageViewModel();
+        }
+    }
+}
