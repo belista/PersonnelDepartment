160e318e54c3fbf5ff91890be4d09569dcdab2e1
belista <shestakov9999@mail.ru>
2017-11-30 16:22:51 +0300

    Добавлен EmployeeContext, подключение к бд.

---
+++ PersonnelDepartment/Core/Services/DataProvider/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using PersonnelDepartment.Models;
+using System.Data.Entity;
+
+namespace PersonnelDepartment.Core.Services.DataProvider
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext() 
+            :base("DefaultConnection")
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
