0fca4258bf01315d6699ddbff6ec67c37fcfcbbb
belista <shestakov9999@mail.ru>
2017-11-30 16:42:50 +0300

    Добавлен BindableBase.

---
+++ PersonnelDepartment/Mvvm/BindableBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PersonnelDepartment.Mvvm
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        protected void Set<T>(ref T prop, T value, [CallerMemberName]string name = null)
+        {
+            if (Equals(prop, value))
+            {
+                return;
+            }
+
+            prop = value;
+            OnPropertyChanged(name);
+        }
+
+        protected void Set<T>(ref T prop, T value, Action callback, [CallerMemberName]string name = null)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException(nameof(callback));
+            }
+
+            if (Equals(prop, value))
+            {
+                return;
+            }
+
+            prop = value;
+            OnPropertyChanged(name);
+            callback.Invoke();
+        }
+    }
+}
